8a484577f48af5b98a5f0a2dc548f5dffcd8bf8a
Alexis Navarro <navarroalexis007@gmail.com>
2025-04-02 23:38:16 -0600

    added comments to explain has duplicate function

--- array_hashing/easy_arrays.py
+++ array_hashing/easy_arrays.py
@@ -1,9 +1,11 @@
 class Solution:
     #Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
     def hasDuplicate(self, nums: list[int]) -> bool:
+        #create an empty set that will be used to store unique values from the nums array
         dupes = set()
 
         for item in nums:
+            #if the nums val exists in the set then return true, if not add that value to the set and continue checking the array
             if item in dupes:
                 return True
             else:
