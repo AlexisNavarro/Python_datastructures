142366ac5ae146bdb1144dfa8aafa211a5cf05f2
Alexis Navarro <navarroalexis007@gmail.com>
2025-05-17 15:06:43 -0600

    implemented is balanced method with comments

--- binary_trees/easy_binary_trees.py
+++ binary_trees/easy_binary_trees.py
@@ -73,6 +73,29 @@ class Solution:
 
         dfs(root)
         return self.res
+    
+    def isBalanced(self, root):     
+        def dfs(root):
+            if root is None:
+                return [True, 0]
+
+            #traverse the subtrees and count the depth
+            left = dfs(root.left)
+            right = dfs(root.right)
+
+            
+            # Check if the current subtree is balanced:
+            # 1. Left subtree is balanced
+            # 2. Right subtree is balanced
+            # 3. The height difference between left and right is at most 1
+            balanced = (left[0] and right[0] and 
+                abs(left[1]-right[1]) <=1)
+                
+            return [balanced, 1 + max(left[1], right[1])]
+
+        return dfs(root)[0]
+
+
         
 
 
